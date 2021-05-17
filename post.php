f657008fe9966c8fae37fe23850a6c2d893f1e2b
Melvin More <melvinm@m2north.com>
2021-05-17 09:11:57 +0200

    post updated

--- post.php
+++ post.php
@@ -1,14 +1,13 @@
 <?php
 include 'repository.php';
-/* 
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); */
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 http_response_code(200);
-print_r($_POST);
 $repository = new Repository();
 $qualifications = json_decode($_POST['qualifications']);
 $qualification_id = json_decode($_POST['qualification']);
